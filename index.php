f1abf0ce514eea22401aa6c79c220fbc0093a4e3
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-02-15 02:06:33 +0900

    index page introduction

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container mt-5">
+      <form action="GET">
+        <div class="form-group">
+          <h2>Add participants</h2>
+          <input type="text" class="form-control" placeholder="Name of paricipants" name="names">
+          <small id="namesHelp" class="form-text text-muted">Add the names separated by commas</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Add participants</button>
+      </form>
+    </div>
+  </body>
+</html>
